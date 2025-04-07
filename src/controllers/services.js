bad6c7a0ebd2a700fbc072d557827b10353c4d24
WilliamD3v <williamD3v@gmail.com>
2025-04-07 13:54:33 -0300

    Sistema de atualizacao de dados de seviços implementada

--- src/controllers/services.js
+++ src/controllers/services.js
@@ -1,7 +1,9 @@
 import express from "express";
+
 import { registerService } from "../server/services";
-import { getServiceByClientId } from "../server/services";
+import { updataService } from "../server/services";
 import { deleteService } from "../server/services";
+import { getServiceByClientId } from "../server/services";
 
 const router = express.Router();
 
@@ -17,16 +19,17 @@ router.post("/register/:userId", async (req, res) => {
   }
 });
 
-router.get("/data-service/:userId", async (req, res) => {
-  const userId = req.params.userId;
+router.put("/update-service/:serviceId", async (req, res) => {
+  const body = req.body;
+  const serviceId = req.params.serviceId;
+
+  console.log(body)
 
   try {
-    const results = await getServiceByClientId(userId);
-    res.status(201).json(results);
+    const results = await updataService(body, serviceId);
+    res.status(200).json(results);
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Erro ao cadastrar serviço", error: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 
@@ -39,6 +42,19 @@ router.delete("/delete/:userId/:serviceId", async (req, res) => {
     res.status(201).json(results);
   } catch (error) {
     res.status(500).json(error, "Error ao cadastrar serviço");
+  }
+});
+
+router.get("/data-service/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const results = await getServiceByClientId(userId);
+    res.status(201).json(results);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao cadastrar serviço", error: error.message });
   }
 });
 
--- src/server/services.js
+++ src/server/services.js
@@ -15,23 +15,45 @@ export const registerService = async (body, userId) => {
   return createService;
 };
 
-export const getServiceByClientId = async (userId) => {
-  try {
-    await databaseConnection();
+export const updataService = async (body, serviceId) => {
+  await databaseConnection()
 
-    const services = await servicoSchema.find({ barbearia_id: userId });
-
-    if (!services || services.length === 0) {
-      console.log("Serviços não encontrados para essa barbearia");
-      return { message: "Nenhum serviço encontrado" }; // Retorne uma mensagem para o cliente
-    }
-
-    return services;
-  } catch (error) {
-    console.error("Erro ao buscar serviços:", error);
-    throw new Error("Falha ao buscar serviços");
+  const existingService = await servicoSchema.findById({ _id: serviceId })
+  if (!existingService) {
+    throw new Error("Serviço não encontrado");
   }
-};
+
+  const updates = {};
+
+  if (body.name && body.name !== existingService.name) {
+    updates.name = body.name;
+  }
+
+  if (typeof body.price === "number" && body.price !== existingService.price) {
+    updates.price = body.price;
+  }
+
+  if (body.description && body.description !== existingService.description) {
+    updates.description = body.description;
+  }
+
+  const durationNumber = Number(body.duration);
+  if (!isNaN(durationNumber) && durationNumber !== existingService.duration) {
+    updates.duration = durationNumber;
+  }
+
+  if (Object.keys(updates).length === 0) {
+    return { message: "Nenhuma alteração detectada." };
+  }
+
+  const updated = await servicoSchema.findByIdAndUpdate(
+    serviceId,
+    { $set: updates },
+    { new: true }
+  );
+
+  return updated;
+}
 
 export const deleteService = async (userId, serviceId) => {
   await databaseConnection()
@@ -56,3 +78,22 @@ export const deleteService = async (userId, serviceId) => {
   console.log("Serviço deletado com sucesso:", result);
   return result
 }
+
+
+export const getServiceByClientId = async (userId) => {
+  try {
+    await databaseConnection();
+
+    const services = await servicoSchema.find({ barbearia_id: userId });
+
+    if (!services || services.length === 0) {
+      console.log("Serviços não encontrados para essa barbearia");
+      return { message: "Nenhum serviço encontrado" }; // Retorne uma mensagem para o cliente
+    }
+
+    return services;
+  } catch (error) {
+    console.error("Erro ao buscar serviços:", error);
+    throw new Error("Falha ao buscar serviços");
+  }
+};
